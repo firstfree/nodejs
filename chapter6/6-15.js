be151235d247473ac48637ee9df4ac8859b2e2a5
JesusOh <ojsboy@hotmail.com>
2016-11-10 06:47:01 +0900

    repeat

--- chapter6/6-15.js
+++ chapter6/6-15.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function(request, response) {
-    response.writeHead(302, { 'Location': 'http://www.hanbit.co.kr' });
+    response.writeHead(302, { 'Location': 'http://www.hanbit.co.kr'});
     response.end();
 }).listen(52273, function() {
     console.log('Server Running at http://127.0.0.1:52273');
--- chapter6/6-28.js
+++ chapter6/6-28.js
@@ -19,6 +19,7 @@ http.createServer(function(request, response) {
 
         response.end('<h1>쿠키를 생성했습니다.</h1>')
     }
+
 }).listen(52273, function() {
     console.log('Server Running at http://127.0.0.1:52273');
 });
